22bf46dd0a47d8cf9cc7d05024879c634d8f2f0a
Nakshatra-14 <nakshatranaskar4762@gmail.com>
2024-10-17 21:00:13 +0530

    update

--- Array2D.java
+++ Array2D.java
@@ -13,6 +13,29 @@ public class Array2D {
                         {4, 5, 6}, 
                         {7, 8, 9}
                       };  
-        printArray(arr);                  
+
+        System.out.println("Length of the rows: " + arr.length);
+        System.out.println("Length of the column: " + arr[0].length);
+
+        System.out.println("Using Function:");
+        printArray(arr);    
+
+        System.out.println("\nUsing for loop: ");
+        for(int i = 0 ; i < arr.length ; i++)
+        {
+            for(int j = 0 ; j < arr[i].length ; j++)
+            {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("\nUsing for each loop:");
+        //using for each loop
+        for (int row[] : arr) {
+            for (int e : row) {
+                System.out.print(e + " ");
+            }
+            System.out.println();
+        }
     }
 }
