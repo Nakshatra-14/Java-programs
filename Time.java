6ecebf1bcb877afcfb5ce68ed89e38912ccaebb3
Nakshatra-14 <nakshatranaskar4762@gmail.com>
2024-11-02 19:40:01 +0530

    add Time

--- Time.java
+++ Time.java
@@ -1,3 +1,10 @@
 public class Time {
-    
+    public static void main(String[] args) {
+        System.out.println("Time from 1 Jan 1900:");
+        System.out.println(System.currentTimeMillis() / 1000 + " milliseconds");
+        System.out.println(System.currentTimeMillis() / 1000 / 3600 / 24 + " days");
+        System.out.println(System.currentTimeMillis() / 1000 / 3600 / 24 / 7 + " weeks");
+        System.out.println(System.currentTimeMillis() / 1000 / 3600 / 24 / 30 + " months");
+        System.out.println(System.currentTimeMillis() / 1000 / 3600 / 24 / 365 + " years");
+    }
 }
