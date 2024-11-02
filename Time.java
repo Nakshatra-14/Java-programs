99d657744a0a465c24b5447bacee28c4fae97c1b
Nakshatra-14 <nakshatranaskar4762@gmail.com>
2024-11-02 23:11:51 +0530

    update

--- Time.java
+++ Time.java
@@ -1,10 +1,26 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Time {
     public static void main(String[] args) {
-        System.out.println("Time from 1 Jan 1900:");
-        System.out.println(System.currentTimeMillis() / 1000 + " milliseconds");
-        System.out.println(System.currentTimeMillis() / 1000 / 3600 / 24 + " days");
-        System.out.println(System.currentTimeMillis() / 1000 / 3600 / 24 / 7 + " weeks");
-        System.out.println(System.currentTimeMillis() / 1000 / 3600 / 24 / 30 + " months");
-        System.out.println(System.currentTimeMillis() / 1000 / 3600 / 24 / 365 + " years");
+        
+        // System.out.println("Time from 1 Jan 1900:");
+        // System.out.println(System.currentTimeMillis() / 1000 + " milliseconds");
+        // System.out.println(System.currentTimeMillis() / 1000 / 3600 / 24 + " days");
+        // System.out.println(System.currentTimeMillis() / 1000 / 3600 / 24 / 7 + " weeks");
+        // System.out.println(System.currentTimeMillis() / 1000 / 3600 / 24 / 30 + " months");
+        // System.out.println(System.currentTimeMillis() / 1000 / 3600 / 24 / 365 + " years");
+
+
+        LocalDateTime dt = LocalDateTime.now();                                         //Date
+        // System.out.println(dt);
+
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");       //Formatter
+        String date = dt.format(df);                                                    //Creating Date using Date and Formatter
+        System.out.println(date);
+
+        DateTimeFormatter df2 = DateTimeFormatter.ISO_LOCAL_DATE;
+        date = dt.format(df2);
+        System.out.println(date);
     }
 }
