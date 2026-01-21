dbab21c335f22c0bc03c572219f5577d415b7a08
Nakshatra-14 <nakshatranaskar4762@gmail.com>
2026-01-22 01:14:07 +0530

    update

--- Projects/Regex/src/nn/regex/ImageExtractor.java
+++ Projects/Regex/src/nn/regex/ImageExtractor.java
@@ -59,66 +59,73 @@ public class ImageExtractor {
         return al;
     }
 
-    // public static void replaceLinks(File fSource, String tSource) throws IOException {
-    //     String regex = ".*^(https|http)[:]...*[?](text=(.*))";
-    //     Pattern pat = Pattern.compile(regex);
-    //     String str = "";
-    //     try (
-    //             Scanner sc = new Scanner(fSource);) {
-    //         while (sc.hasNextLine()) {
-    //             String line = sc.nextLine();
-    //             Matcher matcher = pat.matcher(line);
-    //             if (matcher.find()) {
-    //                 // System.out.println("http://our-server.co.org/imgs/" + matcher.group(3));
-    //                 String l = "http://our-server.co.org/imgs/" + matcher.group(3) + "\n";
-    //                 str += l;
-    //             } else
-    //                 str += line;
-    //         }
-    //         // System.out.println(str);
-    //     }
-    //     File fTarget = new File(tSource);
-    //     if (!fTarget.exists()) {
-    //         FileOutputStream fos = new FileOutputStream(fTarget);
-    //         fos.close();
-    //     }
-
-    //     try (
-    //             FileWriter fw = new FileWriter(fTarget);) {
-    //         fw.write(str);
-    //     }
+    // public static void replaceLinks(File fSource, String tSource) throws
+    // IOException {
+    // String regex = ".*^(https|http)[:]//.*[?](text=(.*))";
+    // Pattern pat = Pattern.compile(regex);
+    // String str = "";
+    // try (
+    // Scanner sc = new Scanner(fSource);) {
+    // while (sc.hasNextLine()) {
+    // String line = sc.nextLine();
+    // Matcher matcher = pat.matcher(line);
+    // if (matcher.find()) {
+    // // System.out.println("http://our-server.co.org/imgs/" + matcher.group(3));
+    // String l = "http://our-server.co.org/imgs/" + matcher.group(3) + "\n";
+    // str += l;
+    // } else
+    // str += line;
+    // }
+    // // System.out.println(str);
+    // }
+    // File fTarget = new File(tSource);
+    // if (!fTarget.exists()) {
+    // FileOutputStream fos = new FileOutputStream(fTarget);
+    // fos.close();
     // }
 
-    public static void replaceLinks(File fSource, String tSource) throws IOException {
-        String regexForImgTag = "<img.*(src\\s*=\\s*\\\"([^\"]*)\\\").*>";
-        String regexForLink = ".*^(https|http)[:]...*[?](text=(.*))";
-        Pattern patImgTag = Pattern.compile(regexForImgTag);
-        Pattern patForLink = Pattern.compile(regexForLink);
+    // try (
+    // FileWriter fw = new FileWriter(fTarget);) {
+    // fw.write(str);
+    // }
+    // }
+
+    public static void replaceLinks(File fSource, File fTarget) throws IOException {
+        String regexForGetImgTag = "(<img.*)(src\\s*=\\s*\\\"([^\\\"]*)\\\")(.*>)";
+        String regexForGetLink = ".*(https|http)[:]...*[?](text=(.*))";
+        Pattern patImgTag = Pattern.compile(regexForGetImgTag);
+        Pattern patForLink = Pattern.compile(regexForGetLink);
         String str = "";
         try (
                 Scanner sc = new Scanner(fSource);) {
             while (sc.hasNextLine()) {
                 String line = sc.nextLine();
-                Matcher matcher = patImgTag.matcher(line);
-                if (matcher.find()) {
-                    al.add(matcher.group(2));
-                }
+                Matcher matcherForImgTag = patImgTag.matcher(line);
+                if (matcherForImgTag.find()) {
+                    Matcher matcherForLink = patForLink.matcher(line);
+                    // System.out.println(line);
+                    if (matcherForLink.find()) {
+                        String s = matcherForImgTag.group(1) + "src=" + "\"" +
+                                "http://our-server.co.org/imgs/" + matcherForLink.group(3) + "\"" +
+                                matcherForImgTag.group(4) + "\n";
+                        System.out.println(s);
+                        str += s;
+                    }
+                } else
+                    str += line + "\n";
             }
+            // System.out.println(str);
         }
-        // System.out.println(str);
-    }
+        // File fTarget = new File(tSource);
+        // if (!fTarget.exists()) {
+        // FileOutputStream fos = new FileOutputStream(fTarget);
+        // fos.close();
+        // }
 
-    File fTarget = new File(tSource);if(!fTarget.exists())
-    {
-        FileOutputStream fos = new FileOutputStream(fTarget);
-        fos.close();
-    }
-
-    try(
-    FileWriter fw = new FileWriter(fTarget);)
-    {
+        try (
+        FileWriter fw = new FileWriter(fTarget);) {
         fw.write(str);
-    }
+        }
     }
 
     public static void main(String[] args) throws IOException {
@@ -126,23 +133,24 @@ public class ImageExtractor {
         // List<String> al = getImgTag(new File("index.html"));
 
         // for(String s : al)
-        //     System.out.println(s);
+        // System.out.println(s);
         // System.out.println();
         // Collections.sort(al);
 
         // for(String s : al)
-        //     System.out.println(s);
-
+        // System.out.println(s);
 
         // https://via.placeholder.com/200x150?text=Goofy
         // http://our-server.co.org/images/Goofy.jpg
 
         // https://www.google.com/search?q=mickey+mouse+jpg+pic&text=gallery&oq=mickey+mouse+jpg+pic&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIJCAEQABgNGIAEMggIAhAAGBYYHjIICAMQABgWGB4yCAgEEAAYFhgeMgoIBRAAGIAEGKIEMgoIBhAAGIAEGKIEMgoIBxAAGIAEGKIEMgoICBAAGIAEGKIEMgoICRAAGIAEGKIE0gEKMTUxOTNqMGoxNagCCLACAfEFiLX_dbUj8X4&sourceid=chrome&ie=UTF-8
-        
-        //WAP that inp a html file and copy that file to another html, with all the links in the img source, replaced as bellow
-        //http://our-server.co.org/imgs/<imagefilename>.jpg
-        //where img file name is txt as before
 
-        replaceLinks(new File("T.txt"), "new.txt");
+        // WAP that inp a html file and copy that file to another html, with all the
+        // links in the img source, replaced as bellow
+        // http://our-server.co.org/imgs/<imagefilename>.jpg
+        // where img file name is txt as before
+
+        // replaceLinks(new File("T.txt"), "new.txt");
+        replaceLinks(new File("index.html"), new File("new.html"));
     }
 }
