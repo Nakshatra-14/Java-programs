4456a9659b800c5224af9bddd69e8ae894e29b72
Nakshatra-14 <nakshatranaskar4762@gmail.com>
2026-01-26 22:36:56 +0530

    astro

--- Projects/Astro/src/nn/astro/GetPlanetDetails.java
+++ Projects/Astro/src/nn/astro/GetPlanetDetails.java
@@ -1,13 +1,31 @@
 package nn.astro;
 
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class GetPlanetDetails extends JFrame{
 
-    private static List<Planets> al = new ArrayList<>();
-
+    private List<Planets> al = new ArrayList<>();
+    private JTextField txtElement = new JTextField(20);
+    private JTextField txtNature = new JTextField(20);
+    private JLabel showColor = new JLabel("  Hello                                  ");
+    private JTextArea txtArInfo = new JTextArea(4, 20);
+ 
     public static String getPlanetName(int n)
     {
         return switch (n) {
@@ -24,7 +42,7 @@ public class GetPlanetDetails extends JFrame{
         };
     }
 
-    public static String getPlanetColor(String name)
+    public static String getPlanetColorString(String name)
     {
         return switch(name)
         {
@@ -41,24 +59,7 @@ public class GetPlanetDetails extends JFrame{
         };
     }
 
-    public String getInfo(String name)
-    {
-        return switch(name)
-        {
-            case "Sun" -> "orange, father king, slow, honesty, politician, royality, fire, doctor, heart, head, acidity, eight eye";
-            case "Moon" -> "white, mother, food, water, kindness, nurse, mind and mental, broncrities, numonia, insomia, husband";
-            case "Mars" -> "general, tough, adament, red, machinary, enginering, fever, dirriodia, brother, land, property";
-            case "Mercury" -> "green greenery, talkative, diluted, book, writing, reading, school, baby nature, baby items, sister, hand, ear, arm";
-            case "Jupiter" -> "yellow, teacher, god, temple, higher study, growth (cancer), jandice";
-            case "Venus" -> "firoji, soild, filter, purify, skin disease, kidney, wife";
-            case "Saturn" -> "black, aged person, laubour, begger, servent, delay, bone, hair, farmer property, job, teeth, leg";
-            case "Rahu" -> "online, gadgets, big thinking, focus anything, dart board, wind chimes, camera, hot drinks, upper portion of body, dirty";
-            case "Ketu" -> "pale brown, small place, narrow, secret, detach, lower portion of body, nature, meditation, yoga, cyst, back pain, low pressure";
-            default -> "Invalid";
-        };
-    }
-
-    public static void addPlanetsDetails()
+    public void addPlanetsChartDetails()
     {
         al.add(new Planets(getPlanetName(3), 1));
         al.add(new Planets(getPlanetName(6), 2));
@@ -74,23 +75,105 @@ public class GetPlanetDetails extends JFrame{
         al.add(new Planets(getPlanetName(5), 12));
     }
 
-    // GetPlanetDetails()
-    // {
-    //     this.setTitle("Get Planet details");
-    //     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public String[] getPlanetNameArray()
+    {
+        ArrayList<String> l = new ArrayList<>();
+        for(int i = 0 ; i < 12 ; i++)
+            l.add(i+1 + ": " + al.get(i).getName());
+        return l.toArray(new String[al.size()]);
+    }
 
-    //     this.add(new JButton("Helloo"));
-    // }
+    GetPlanetDetails()
+    {
+        this.setTitle("Get Planet details");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        showColor.setBackground(Color.pink);
+        showColor.setOpaque(true);
+        addPlanetsChartDetails();
 
-    // public static void main(String[] args) {
-    //     var app = new GetPlanetDetails();
-    //     app.pack();
-    //     app.setLocationRelativeTo(null);
-    //     app.setVisible(true);
-    // }
+        // JList<String> lst = new JList<>(getPlanetNameArray());
+        JComboBox<String> cmdbox= new JComboBox<>(getPlanetNameArray());
+        addActionListnerToComboBox(cmdbox);
+
+        cmdbox.setSelectedIndex(0);
+
+        JPanel p = new JPanel();
+        addComponentToPanel(p, cmdbox, new JLabel("Element:"), new JLabel("Nature:"), new JLabel("Color:"), new JLabel("Info"), txtElement, txtNature, showColor, txtArInfo);
+        this.add(p);
+    }
 
     public static void main(String[] args) {
-        addPlanetsDetails();
-        System.out.println(al);
+        var app = new GetPlanetDetails();
+        app.pack();
+        app.setLocationRelativeTo(null);
+        app.setVisible(true);
     }
+
+    public Color getPlanetColour(String name)
+    {
+        return switch(name)
+        {
+            case "Sun" -> Color.ORANGE;
+            case "Moon" -> Color.WHITE;
+            case "Mars" -> Color.RED;
+            case "Mercury" -> Color.GREEN;
+            case "Jupiter" -> Color.YELLOW;
+            case "Venus" -> new Color(0, 168, 173);
+            case "Saturn" -> Color.BLACK;
+            case "Rahu" -> Color.GRAY;
+            case "Ketu" -> new Color(152, 118, 84);
+            default -> Color.pink;
+        };
+    }
+
+    public void addActionListnerToComboBox(JComboBox<String> cmdbox)
+    {
+        cmdbox.addActionListener( _ -> {
+            int n = cmdbox.getSelectedIndex();
+            System.out.println(al.get(n));
+            String name = al.get(n).getName();
+            txtElement.setText(al.get(n).getElement());
+            txtNature.setText(al.get(n).getNature());
+            showColor.setText(" ".repeat(8) + getPlanetColorString(name) + " ".repeat(8));
+            showColor.setBackground(getPlanetColour(name));
+            txtArInfo.setText(al.get(n).getInfo());
+        });
+    }
+
+
+
+    public void addComponentToPanel(JPanel p, JComboBox<String> cmdBox, JLabel lblElement, JLabel lblNature, JLabel lblColor, JLabel lblInfo, JTextField txtElement, JTextField txtNature, JLabel txtColor, JTextArea txtInfo)
+    {
+        p.setLayout(new GridBagLayout());
+        Insets insets = new Insets(5,5, 5, 5);
+
+        GridBagConstraints gbc = new GridBagConstraints(1, 0, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, insets, 0, 0);
+        p.add(cmdBox, gbc);
+
+        gbc = new GridBagConstraints(0, 1, 1, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.EAST, insets, 0, 0);
+        p.add(lblElement, gbc);
+
+        gbc = new GridBagConstraints(0, 2, 1, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.EAST, insets, 0, 0);
+        p.add(lblNature, gbc);
+
+        gbc = new GridBagConstraints(0, 3, 1, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.EAST, insets, 0, 0);
+        p.add(lblColor, gbc);
+
+        gbc = new GridBagConstraints(0, 4, 1, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.NONE, insets, 0, 0);
+        p.add(lblInfo, gbc);
+
+        gbc = new GridBagConstraints(1, 1, 1, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.EAST, insets, 0, 0);
+        p.add(txtElement, gbc);
+
+        gbc = new GridBagConstraints(1, 2, 1, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.EAST, insets, 0, 0);
+        p.add(txtNature, gbc);
+
+        gbc = new GridBagConstraints(1, 3, 1, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.EAST, insets, 0, 0);
+        p.add(txtColor, gbc);
+
+        gbc = new GridBagConstraints(1, 4, 2, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.EAST, insets, 0, 0);
+        p.add(txtInfo, gbc);
+
+    }
+
 }
--- Projects/Astro/src/nn/astro/Planets.java
+++ Projects/Astro/src/nn/astro/Planets.java
@@ -9,7 +9,6 @@ public class Planets {
     private String color;
     private String nature;
     private String element;
-    private String bodyPart;
     private String info;
 
     String natureContainer[] = {"movable", "fixed", "duel", "movable", "fixed", "duel", "movable", "fixed", "duel", "movable", "fixed", "duel"};
@@ -34,12 +33,51 @@ public class Planets {
         this.color = clr;
         this.nature = natureContainer[number-1];
         this.element = elementContainer[number-1];
+
+        String str = switch(name)
+        {
+            case "Sun" -> "orange, father king, slow, \nhonesty, politician,royality, \nfire, doctor, heart, head, acidity, \neight eye";
+            case "Moon" -> "white, mother, food, water, \nkindness, nurse, mind and mental, \nbroncrities, numonia, insomia, husband";
+            case "Mars" -> "general, tough, adament, red,\nmachinary, enginering, fever, \ndirriodia, brother, land, property";
+            case "Mercury" -> "green greenery, talkative, \ndiluted, book, writing, reading, \nschool, baby nature, baby items, \nsister, hand, ear, arm";
+            case "Jupiter" -> "yellow, teacher, god, temple, higher \nstudy, growth (cancer), jandice";
+            case "Venus" -> "firoji, soild, filter, purify, \nskin disease, kidney, wife";
+            case "Saturn" -> "black, aged person, laubour, \nbegger, servent, delay, bone, hair, \nfarmer property, job, teeth, leg";
+            case "Rahu" -> "online, gadgets, big thinking, \nfocus anything, dart board, \nwind chimes, camera, hot drinks, \nupper portion of body, dirty";
+            case "Ketu" -> "pale brown, small place, narrow, \nsecret, detach, lower portion of body, \nnature, meditation, yoga, cyst, \nback pain, low pressure";
+            default -> "Invalid";
+        };
+        this.info = str;
     }
 
     
     @Override
     public String toString() {
         return "Planets [number=" + number + ", name=" + name + ", color=" + color + ", nature=" + nature + ", element="
-                + element + "]\n";
+                + element + ", info = " + info +"]\n";
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getNature() {
+        return nature;
+    }
+
+    public String getElement() {
+        return element;
+    }
+
+    public String getInfo() {
+        return info;
     }
 }
