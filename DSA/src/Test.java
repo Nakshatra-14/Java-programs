4111872db7382125a0046f28c9942f2a58a4b1ac
Nakshatra-14 <nakshatranaskar4762@gmail.com>
2026-03-29 23:22:10 +0530

    update

--- DSA/src/Test.java
+++ DSA/src/Test.java
@@ -1,25 +1,38 @@
 
-import java.util.function.Function;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
-interface A 
-{
+interface A {
     void show();
 }
 
 public class Test {
-    public static void main(String[] args) {
-        // A obj =  () -> System.out.println("Helloo");
+    public static void main(String[] args) throws IOException {
+        // A obj = () -> System.out.println("Helloo");
         // obj.show();
 
-        // Function<Integer, String> f = (integer) ->  {return String.valueOf(integer);};
+        // Function<Integer, String> f = (integer) -> {return String.valueOf(integer);};
         // System.out.println(f.apply(10));
+
+        List<Integer> lst = new ArrayList<>(1000);
+
+        Random rnd = new Random();
+
+        for(int i = 0 ; i < lst.size() ; i++)
+            lst.add(rnd.nextInt(100));
+
+        lst.forEach(e -> System.out.println(e));
+
+        System.out.println("worked");
     }
 }
 
 // class Func implements Function<Integer, String>
 // {
-//     @Override
-//     public String apply(Integer t) {
-//         return String.valueOf(t);
-//     }
+// @Override
+// public String apply(Integer t) {
+// return String.valueOf(t);
+// }
 // }
--- DSA/src/stack/StackWithLinkedList.java
+++ DSA/src/stack/StackWithLinkedList.java
@@ -1,6 +1,7 @@
 package stack;
 
 import java.util.LinkedList;
+import java.util.StringJoiner;
 
 public class StackWithLinkedList<T> implements MyStack<T>
 {
@@ -31,6 +32,33 @@ public class StackWithLinkedList<T> implements MyStack<T>
     @Override
     public T peek() {
         return lst.peek();
+    }
+
+    @Override
+    public String toString()
+    {
+        StringJoiner sj = new StringJoiner(", ", "[", "]") ;
+
+        for(T e : lst)
+        {
+            sj.add(e.toString()) ;
+        }
+
+        return sj.toString() ;
+    }
+
+
+    public static void main(String[] args) {
+        StackWithLinkedList<Integer> lst = new StackWithLinkedList<>();
+
+        lst.push(1);
+        lst.push(2);
+        lst.push(3);
+        lst.push(4);
+        System.out.println(lst.pop());
+        lst.push(5);
+        lst.push(6);
+        System.out.println(lst.toString());
     }
 
 }
